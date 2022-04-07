f3933606e0fdf5487792216dd8e508b0c4ce96cf
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-07 10:12:56 -0400

    evaluation script

--- evaluate_agent.py
+++ evaluate_agent.py
@@ -2,30 +2,15 @@ import gym
 
 import argparse
 import importlib
-import random
 import numpy as np
-from time import time
-from datetime import datetime
-
-import tensorflow as tf
-import torch
 
 import os
 from os import listdir, makedirs
 from os.path import isfile, join
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import auc
-
 from environments import JellyBeanEnv, MujocoEnv
 
-class Logger():
-    def __init__(self, path):
-        self.logfile = open(path+'/log.txt',"w+")
-        self.location = path
-    def log(self, msg):
-        print(msg)
-        self.logfile.write(f'\n{msg}')
+from utils.logging import start_logging
 
 
 def evaluate_agent(agent, env, n_episodes_to_evaluate):
@@ -55,28 +40,6 @@ def get_environment(env_type):
             "ERROR: Please define your env_type to be either 'jellybean' or 'mujoco'!"
         )
     return env
-
-
-def start_logging():
-    '''
-    Starts an instance of the Logger class to log the training results. 
-    :return logger: the Logger class instance for this training session
-    :return results_path: the location of the log files
-    '''
-    # making sure there is a results folder
-    results_folder = os.path.join(os.getcwd(),'results')
-    if not os.path.isdir(results_folder):
-        os.mkdir(results_folder)
-
-    # making a folder for this current training
-    results_path = os.path.join(os.getcwd(),'results/' + datetime.now().strftime('%Y-%m-%d_%Hh%Mm%S'))
-    if not os.path.isdir(results_path):
-        os.mkdir(results_path)
-
-    # start an instance of the Logger class :)
-    logger = Logger(results_path)
-
-    return logger
 
 
 if __name__ == "__main__":
@@ -125,17 +88,14 @@ if __name__ == "__main__":
         logger.log(f'Pretrained model loaded: {args.load}')
 
     # "Out-of-sample" Evaluation 
-    n_evaluations = 1000
-    n_episodes_to_evaluate = 50
+    n_episodes_to_evaluate = 100
+
+    # TODO: Add Sabina's sample efficiency calculation here?
 
     ########################################## evaluate a single model ##########################################
     logger.log("Evaluation starting ... ")
-    start_time = time()
+    # Calculate the average (out-of-sample) reward
     average_reward_list = []
-    for i in range(n_evaluations):
-        average_evaluation_reward = evaluate_agent(agent, env_eval, n_episodes_to_evaluate)
-        if i % 100 == 0:
-            logger.log(f"Average reward for {args.load}: {np.mean(np.array(average_reward_list))}")
-        average_reward_list.append(average_evaluation_reward)
-    final_average_reward = np.mean(np.array(average_reward_list))
-    logger.log(f"Evaluation complete. Final average reward: {final_average_reward}")
+    average_reward_per_episode = evaluate_agent(agent, env_eval, n_episodes_to_evaluate)
+
+    logger.log(f"Average reward per episode: {average_reward_per_episode} +/-")
