d376f96142e41d1fa8b739aae5146c0ec6a3fab8
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-11 15:14:06 -0400

    save_checkpoint=False to sample efficiency

--- evaluate_agent.py
+++ evaluate_agent.py
@@ -117,8 +117,7 @@ if __name__ == "__main__":
             evaluation_freq,
             n_episodes_to_evaluate_sample_efficiency,
             num_seeds,
-            logger,
-            save_checkpoint=False
+            logger
         )
         logger.log(f"Sample efficiency: {sample_efficiency}")
         logger.log(f"Time to train: {time_to_train}")    
@@ -134,8 +133,7 @@ if __name__ == "__main__":
             evaluation_freq,
             n_episodes_to_evaluate_sample_efficiency,
             num_seeds,
-            logger,
-            save_checkpoint=False
+            logger
         )
         # Calculate the average (out-of-sample) reward
         logger.log("Evaluating the average return of the pretrained model ... ")
@@ -159,8 +157,7 @@ if __name__ == "__main__":
                 evaluation_freq,
                 n_episodes_to_evaluate_sample_efficiency,
                 num_seeds,
-                logger,
-                save_checkpoint=False
+                logger
             )
             sample_efficiencies.append(sample_efficiency)
             times_to_train.append(time_to_train)
