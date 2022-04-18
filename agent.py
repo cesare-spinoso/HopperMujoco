83223fffcde9884559f30f11551ee066fa9835bb
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-18 12:25:34 -0400

    submit mediocre agent

--- agent.py
+++ agent.py
@@ -1,7 +1,9 @@
+from ast import Return
 import os
 import torch
 from torch import nn
 from torch.functional import F
+from torch.distributions import Normal
 import numpy as np
 
 from typing import Tuple
@@ -12,6 +14,8 @@ class Agent:
     """The agent class that is to be filled.
     You are allowed to add any method you
     want to this class.
+
+    Implemented following the pseudocode here: https://spinningup.openai.com/en/latest/algorithms/sac.html
     """
 
     def __init__(
@@ -20,21 +24,31 @@ class Agent:
         gamma: float = 0.99,
         polyak: float = 0.995,
         q_lr: float = 1e-3,
-        q_architecture: tuple = (64, 64),
-        q_activation_function: F = nn.ReLU,
+        q_architecture: tuple = (400, 300),
+        q_activation_function: F = nn.Tanh,
         policy_lr: float = 1e-3,
-        policy_architecture: tuple = (64, 64),
-        policy_activation_function: F = nn.ReLU,
+        policy_architecture: tuple = (400, 300),
+        policy_activation_function: F = nn.Tanh,
         buffer_size: int = 1_000_000,
-        action_noise_scale: float = 0.1,
-        action_noise_rescaling: int = 1,
-        action_noise_rescaling_frequency: int = 500_000,
+        alpha: float = 0.2,
         exploration_timesteps: int = 10_000,
         update_frequency_in_episodes: int = 50,
         update_start_in_episodes: int = 1_000,
         number_of_batch_updates: int = 1_000,
         batch_size: int = 100,
     ):
+        """Creates an SAC agent. Some of the more obscure parameters are explained below.
+
+        Args:
+            alpha (float, optional): Fixed KL threshold. The Udemy implementation uses a variable KL. Defaults to 0.2.
+            exploration_timesteps (int, optional): How many timesteps does the agent use at the beginning
+            for uniform exploration. Defaults to 10_000.
+            update_frequency_in_episodes (int, optional): Frequency (in episodes) of the number of times that
+            the agent takes gradient steps. Defaults to 50.
+            update_start_in_episodes (int, optional): NUmber of episodes required before the agent starts taking gradient
+            steps for its networks. This is mostly here to ensure that the buffer is full enough to batching. Defaults to 1_000.
+            number_of_batch_updates (int, optional): Number of gradient updates to take. Defaults to 1_000.
+        """
         ### ENVIRONMENT VARIABLES ###
         self.env_specs = env_specs
         # Number of observations (states) and actions
@@ -50,10 +64,7 @@ class Agent:
         ### HYPERPARAMETERS ###
         self.gamma = gamma
         self.polyak = polyak
-        # For the rescaling of noise in the exploration phase
-        self.action_noise_scale = action_noise_scale
-        self.action_noise_rescaling = action_noise_rescaling
-        self.action_noise_rescaling_frequency = action_noise_rescaling_frequency
+        self.alpha = alpha  # entropy parameter
         # Number of time steps where sample actions randomly
         self.exploration_timesteps = exploration_timesteps
         # Frequency, start and size of updates
@@ -61,17 +72,26 @@ class Agent:
         self.update_start_in_episodes = update_start_in_episodes
         self.number_of_batch_updates = number_of_batch_updates
         self.batch_size = batch_size
-        ### Q-NETWORK ###
-        self.q_network = QNetwork(
+        ### Q-NETWORKS (Q1 and Q1) ###
+        self.q1_network = QNetwork(
             num_obs=self.num_obs,
             num_actions=self.num_actions,
             architecture=q_architecture,
             activation_function=q_activation_function,
         )
-        self.q_optimizer = torch.optim.Adam(self.q_network.parameters(), lr=q_lr)
-        # Create q target network and freeze gradients
-        self.q_network_target = deepcopy(self.q_network)
-        self._freeze_network(self.q_network_target)
+        self.q1_optimizer = torch.optim.Adam(self.q1_network.parameters(), lr=q_lr)
+        self.q2_network = QNetwork(
+            num_obs=self.num_obs,
+            num_actions=self.num_actions,
+            architecture=q_architecture,
+            activation_function=q_activation_function,
+        )
+        self.q2_optimizer = torch.optim.Adam(self.q2_network.parameters(), lr=q_lr)
+        # Create q target networks (for both 1 and 2) and freeze gradients
+        self.q1_network_target = deepcopy(self.q1_network)
+        self._freeze_network(self.q1_network_target)
+        self.q2_network_target = deepcopy(self.q2_network)
+        self._freeze_network(self.q2_network_target)
         ### POLICY NETWORK ###
         self.policy_network = PolicyNetwork(
             num_obs=self.num_obs,
@@ -83,15 +103,11 @@ class Agent:
         self.policy_optimizer = torch.optim.Adam(
             self.policy_network.parameters(), lr=policy_lr
         )
-        # Create the target network and freeze its gradients
-        self.policy_network_target = deepcopy(self.policy_network)
-        self._freeze_network(self.policy_network_target)
         ### BUFFER ###
-        self.buffer = DDPGBuffer(
+        self.buffer = SACBuffer(
             number_obs=self.num_obs,
             number_actions=self.num_actions,
             size=buffer_size,
-            gamma=self.gamma,
             batch_size=self.batch_size,
         )
 
@@ -101,7 +117,7 @@ class Agent:
 
         Args:
             root_path (str): Root path
-            pretrained_model_name (str, optional): Model name e.g. vpg_ckpt_98.888. Defaults to None.
+            pretrained_model_name (str, optional): Model name e.g. td3_ckpt_98.888. Defaults to None.
         """
         if pretrained_model_name is None:
             pretrained_model_path = os.path.join(root_path, "submitted_model.pth.tar")
@@ -117,16 +133,15 @@ class Agent:
         except:
             raise Exception(
                 "Invalid location for loading pretrained model. You need folder/filename in results folder (without .pth.tar). \
-                \nE.g. python3 train_agent.py --group vpg_agent --load 2022-03-31_12h46m44/vpg_ckpt_98.888"
+                \nE.g. python3 train_agent.py --group vpg_agent --load 2022-03-31_12h46m44/td3_ckpt_98.888"
             )
 
         # load state dict for the 4 networks
-        self.q_network.load_state_dict(pretrained_model["q_network"])
+        self.q1_network.load_state_dict(pretrained_model["q1_network"])
+        self.q2_network.load_state_dict(pretrained_model["q2_network"])
         self.policy_network.load_state_dict(pretrained_model["policy_network"])
-        self.q_network_target.load_state_dict(pretrained_model["q_network_target"])
-        self.policy_network_target.load_state_dict(
-            pretrained_model["policy_network_target"]
-        )
+        self.q1_network_target.load_state_dict(pretrained_model["q1_network_target"])
+        self.q2_network_target.load_state_dict(pretrained_model["q2_network_target"])
 
         print("Loaded {} OK".format(pretrained_model_name))
 
@@ -139,20 +154,21 @@ class Agent:
         # path for current version you're saving (only need ckpt_xxx, not ckpt_xxx.pth.tar)
         if name == None:
             ckpt_path = os.path.join(
-                ckpt_path, "ddpg_ckpt_" + str(round(score_avg, 3)) + ".pth.tar"
+                ckpt_path, "sac_ckpt_" + str(round(score_avg, 3)) + ".pth.tar"
             )
         else:
             ckpt_path = os.path.join(
                 ckpt_path,
-                "ddpg_ckpt_" + name + "_" + str(round(score_avg, 3)) + ".pth.tar",
+                "sac_ckpt_" + name + "_" + str(round(score_avg, 3)) + ".pth.tar",
             )
 
         torch.save(
             {
-                "q_network": self.q_network.state_dict(),
+                "q1_network": self.q1_network.state_dict(),
+                "q2_network": self.q2_network.state_dict(),
                 "policy_network": self.policy_network.state_dict(),
-                "q_network_target": self.q_network_target.state_dict(),
-                "policy_network_target": self.policy_network_target.state_dict(),
+                "q1_network_target": self.q1_network_target.state_dict(),
+                "q2_network_target": self.q2_network_target.state_dict(),
                 "score": score_avg,
             },
             ckpt_path,
@@ -168,24 +184,13 @@ class Agent:
                     # Do exploration at the beginning of training
                     action = self.env_specs["action_space"].sample()
                 else:
-                    if (
-                        self.current_timestep % self.action_noise_rescaling_frequency
-                        == 0
-                    ):
-                        self.action_noise_scale = (
-                            self.action_noise_scale / self.action_noise_rescaling
-                        )
-                    noise = (
-                        self.action_noise_scale * torch.randn(self.num_actions)
-                    ).to(self.device)
-                    noised_action = self.policy_network(curr_obs) + noise
-                    action = torch.clip(
-                        noised_action, -self.action_limit, self.action_limit
+                    action, _ = self.policy_network(
+                        curr_obs, deterministic=False, return_logprob=False
                     )
                     action = action.data.cpu().numpy()
             else:
-                action = torch.clip(
-                    self.policy_network(curr_obs), -self.action_limit, self.action_limit
+                action, _ = self.policy_network(
+                    curr_obs, deterministic=True, return_logprob=False
                 )
                 action = action.data.cpu().numpy()
             return action
@@ -214,7 +219,7 @@ class Agent:
             )
 
     def train(self):
-        for _ in range(self.number_of_batch_updates):
+        for j in range(self.number_of_batch_updates):
             # Get training batch
             (
                 obs_data,
@@ -223,13 +228,13 @@ class Agent:
                 next_obs_data,
                 done_data,
             ) = self.buffer.get_training_batch()
-            # Get the training targets
+            # Get the training targets (Line 12 of the OpenAI pseudocode)
             y = self.compute_targets(reward_data, next_obs_data, done_data)
-            # Train the Q-network
-            self.train_q_network(obs_data, action_data, y)
-            # Train the policy network
+            # Train the Q-network (Line 13 of the OpenAI pseudocode)
+            self.train_q_networks(obs_data, action_data, y)
+            # Train the policy network (Line 14 of the OpenAI pseudocode)
             self.train_policy_network(obs_data)
-            # Update the target networks
+            # Update the target networks (Line 15 of the OpenAI pseudocode)
             self.update_target_networks()
 
     def compute_targets(
@@ -240,38 +245,51 @@ class Agent:
     ) -> torch.Tensor:
         """Compute the targets for the Q network."""
         with torch.no_grad():
-            return reward_data + self.gamma * (1 - done_data) * self.q_network_target(
-                next_obs_data, self.policy_network_target(next_obs_data)
-            ).squeeze(-1)
+            target_actions, log_proba = self.policy_network(next_obs_data)
+            min_q_network_target = torch.min(
+                self.q1_network_target(next_obs_data, target_actions).squeeze(-1),
+                self.q2_network_target(next_obs_data, target_actions).squeeze(-1),
+            )
+            return reward_data + self.gamma * (1 - done_data) * (
+                min_q_network_target - self.alpha * log_proba
+            )
 
-    def train_q_network(self, obs_data, action_data, y):
-        # Train the Q-network
-        self.q_network.zero_grad()
-        # Compute q-values
-        q_values = self.q_network(obs_data, action_data).squeeze(-1)
-        # Compute loss
-        loss = F.mse_loss(q_values, y)
-        # Backpropagate
-        loss.backward()
-        # Take a step
-        self.q_optimizer.step()
+    def train_q_networks(self, obs_data, action_data, y):
+        for q_network, q_optimizer in [
+            (self.q1_network, self.q1_optimizer),
+            (self.q2_network, self.q2_optimizer),
+        ]:
+            # Train the Q-network
+            q_network.zero_grad()
+            # Compute q-values
+            q_values = q_network(obs_data, action_data).squeeze(-1)
+            # Compute loss
+            loss = F.mse_loss(q_values, y)
+            # Backpropagate
+            loss.backward()
+            # Take a step
+            q_optimizer.step()
 
     def train_policy_network(self, obs_data):
-        # Freeze the Q-network
-        self._freeze_network(self.q_network)
+        # Freeze the Q-networks
+        self._freeze_network(self.q1_network)
+        self._freeze_network(self.q2_network)
         # Train the policy network
         self.policy_network.zero_grad()
-        # Want the policy network to maximize the q-network
-        # so minimize the negative of the q-network
-        q_values = self.q_network(obs_data, self.policy_network(obs_data)).squeeze(-1)
-        # Compute loss
-        loss = -torch.mean(q_values)
+        # Compute the policy target
+        actions, log_proba = self.policy_network(obs_data)
+        q1_values = self.q1_network(obs_data, actions).squeeze(-1)
+        q2_values = self.q2_network(obs_data, actions).squeeze(-1)
+        q_values = torch.min(q1_values, q2_values)
+        # Want to maximize the the output of pi and thus minimize its negative output
+        loss = -(q_values - self.alpha * log_proba).mean()
         # Backpropagate
         loss.backward()
         # Take a step
         self.policy_optimizer.step()
         # Unfreeze the Q-network
-        self._unfreeze_network(self.q_network)
+        self._unfreeze_network(self.q1_network)
+        self._unfreeze_network(self.q2_network)
 
     def _freeze_network(self, network):
         """Freeze the gradients of the network so that loss cannot backprop
@@ -288,8 +306,8 @@ class Agent:
         """Update the target networks for the q-network and the policy-network via
         a moving average."""
         with torch.no_grad():
-            self._polyak_average_update(self.q_network, self.q_network_target)
-            self._polyak_average_update(self.policy_network, self.policy_network_target)
+            self._polyak_average_update(self.q1_network, self.q1_network_target)
+            self._polyak_average_update(self.q2_network, self.q2_network_target)
 
     def _polyak_average_update(self, network, target_network):
         """Polyak averaging of network."""
@@ -332,6 +350,10 @@ class QNetwork(nn.Module):
 
 
 class PolicyNetwork(nn.Module):
+    # Spin-up uses a lower and upper bound for the log_std calculation
+    LOG_STD_MAX = 2
+    LOG_STD_MIN = -20
+
     def __init__(
         self,
         num_obs: int,
@@ -343,31 +365,71 @@ class PolicyNetwork(nn.Module):
         super().__init__()
         """Standard MLP for the policy takes in an observation and outputs an action."""
         super(PolicyNetwork, self).__init__()
+        # Architecture before mean and std output
         layers = [nn.Linear(num_obs, architecture[0]), activation_function()]
         for input_dimension, output_dimension in zip(
             architecture[0:], architecture[1:]
         ):
             layers.append(nn.Linear(input_dimension, output_dimension))
             layers.append(activation_function())
-        layers.append(nn.Linear(architecture[-1], num_actions))
-        # Specific to Mujoco, add tanh activation as the output activation
-        # and rescale it by the action limit
-        self.action_limit = action_limit
-        layers.append(nn.Tanh())
+        # Add a layer for the mean and the std
         self.network = nn.Sequential(*layers)
+        self.mu_layer = nn.Linear(architecture[-1], num_actions)
+        # Output the log for numerical stability
+        self.log_std_layer = nn.Linear(architecture[-1], num_actions)
+
+        self.action_limit = action_limit
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.action_limit * self.network(x)
+    def forward(
+        self, x: torch.Tensor, deterministic=False, return_logprob=True
+    ) -> torch.Tensor:
+        network_output = self.network(x)
+        mu = self.mu_layer(network_output)
+        log_std = self.log_std_layer(network_output)
+        log_std = torch.clamp(
+            log_std, PolicyNetwork.LOG_STD_MIN, PolicyNetwork.LOG_STD_MAX
+        )
+        std = torch.exp(log_std)
+
+        # This is directly from the spin-up implementation
+        # Pre-squash distribution and sample
+        pi_distribution = Normal(mu, std)
+        if deterministic:
+            # Only used for evaluating policy at test time.
+            pi_action = mu
+        else:
+            # Use the re-parametrization trick so that gradients go into the actions,
+            # rsample allows you to do mu + std * noise so that can backprop
+            # through the mu and std layers
+            pi_action = pi_distribution.rsample()
+
+        if return_logprob:
+            # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
+            # NOTE: The correction formula is a little bit magic. To get an understanding
+            # of where it comes from, check out the original SAC paper (arXiv 1801.01290)
+            # and look in appendix C. This is a more numerically-stable equivalent to Eq 21.
+            # Try deriving it yourself as a (very difficult) exercise. :)
+            logp_pi = pi_distribution.log_prob(pi_action).sum(axis=-1)
+            logp_pi -= (2 * (np.log(2) - pi_action - F.softplus(-2 * pi_action))).sum(
+                axis=1
+            )
+        else:
+            logp_pi = None
+
+        # Squash the sampled action and then rescale based on environment
+        pi_action = torch.tanh(pi_action)
+        pi_action = self.action_limit * pi_action
+
+        return pi_action, logp_pi
 
 
-class DDPGBuffer:
+class SACBuffer:
     def __init__(
         self,
         number_obs: int,
         number_actions: int,
-        gamma: float,
         size: int = 1_000_000,
         batch_size: int = 100,
     ) -> None:
@@ -390,7 +452,6 @@ class DDPGBuffer:
         # Device
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # Hyperparameters
-        self.gamma = gamma
         self.batch_size = batch_size
 
     def store_experience(
