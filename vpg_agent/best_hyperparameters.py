2b9b3a06e123f6cc7a191fd3dcffd1c9e0a2ff50
sabina-elkins <sabina@korbit.ai>
2022-04-18 21:12:51 -0400

    stupid variable name error

--- vpg_agent/best_hyperparameters.py
+++ vpg_agent/best_hyperparameters.py
@@ -5,9 +5,9 @@ import sklearn.model_selection
 
 params = {
     "actor_architecture": (64, 64),
-    "actor_activation": torch.nn.ReLU,
+    "actor_activation_function": torch.nn.ReLU,
     "critic_architecture": (64, 64),
-    "critic_activation": torch.nn.ReLU,
+    "critic_activation_function": torch.nn.ReLU,
     "number_of_critic_updates_per_actor_update": 100,
     "batch_size_in_time_steps": 5000,
     "actor_lr": 3e-4,
