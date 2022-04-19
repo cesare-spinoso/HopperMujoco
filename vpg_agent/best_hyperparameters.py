568a6ac25ce0f3fc5efe572aeba3d3bc847184df
sabina-elkins <sabina@korbit.ai>
2022-04-18 21:11:17 -0400

    oops

--- vpg_agent/best_hyperparameters.py
+++ vpg_agent/best_hyperparameters.py
@@ -3,25 +3,14 @@
 import torch
 import sklearn.model_selection
 
-grid = {
-    "architecture": [(64, 64)],
-    "activation": [torch.nn.ReLU],
-    "number_of_critic_updates_per_actor_update": [100],
-    "batch_size_in_time_steps": [5000],
-    "actor_lr": [3e-4],
-    "critic_lr": [1e-3]
+params = {
+    "actor_architecture": (64, 64),
+    "actor_activation": torch.nn.ReLU,
+    "critic_architecture": (64, 64),
+    "critic_activation": torch.nn.ReLU,
+    "number_of_critic_updates_per_actor_update": 100,
+    "batch_size_in_time_steps": 5000,
+    "actor_lr": 3e-4,
+    "critic_lr": 1e-3
 }
-grid = sklearn.model_selection.ParameterGrid(grid)
-
-# Provide activations and architectures to both actor and critic
-hyperparameter_grid = []
-
-for i, params in enumerate(grid):
-    params["actor_architecture"] = params["architecture"]
-    params["actor_activation_function"] = params["activation"]
-    params["critic_architecture"] = params["architecture"]
-    params["critic_activation_function"] = params["activation"]
-    del params["architecture"]
-    del params["activation"]
-    hyperparameter_grid.append(params)
 
