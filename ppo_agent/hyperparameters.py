d7940aea77abd4ae57dde1b79e0c6162319a31dd
sabina-elkins <sabina@korbit.ai>
2022-04-18 20:52:34 -0400

    organizing the updates

--- ppo_agent/hyperparameters.py
+++ ppo_agent/hyperparameters.py
@@ -5,14 +5,9 @@ https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/ppo/ppo.py
 import torch
 import sklearn.model_selection
 
-# grid = {
-#     "architecture": [(64, 64), (100, 50, 25), (400, 300)],
-#     "activation": [torch.nn.ReLU, torch.nn.Tanh]
-# }
-# retraining m_5
-grid = { 
-    "architecture": [(400, 300)],
-    "activation": [torch.nn.Tanh]
+grid = {
+    "architecture": [(64, 64), (100, 50, 25), (400, 300)],
+    "activation": [torch.nn.ReLU, torch.nn.Tanh]
 }
 # gamma = 0.99, lambda = 0.97, KL threshold = 0.015, clip rate = 0.2
 grid = sklearn.model_selection.ParameterGrid(grid)
