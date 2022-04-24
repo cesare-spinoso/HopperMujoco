297b9d0e5916f86b6cb2fe7773213fa9a32fbfc4
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-24 16:21:48 -0400

    save

--- openai_sac_agent/best_hyperparameters.py
+++ openai_sac_agent/best_hyperparameters.py
@@ -7,11 +7,13 @@ import torch
 params = {
     "q_architecture": (64, 64),
     "q_activation_function": torch.nn.ReLU,
+    "q_lr": 1e-3, # Does a higher lr help? It doesn't look like it does no
     "policy_architecture": (64, 64),
     "policy_activation_function": torch.nn.ReLU,
+    "policy_lr": 1e-3,
     "update_alpha": True, # Automatic exploration
-    "exploration_timesteps": 0, # Don't forget
-    "update_start_in_episodes": 50,
-    "update_frequency_in_episodes": 5
+    "exploration_timesteps": 0, # Don't get data that cause you to forget
+    "update_start_in_episodes": 100,
+    "update_frequency_in_episodes": 15 # Increase update frequency
 }
 # the rest are kept as default
