bfc44de4dabea3494b96446c0079559fad78a77f
etiennedemers <74375914+etiennedemers@users.noreply.github.com>
2022-04-23 20:21:42 -0400

    change n timesteps

--- train_agent.py
+++ train_agent.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
         }
 
     # Training and evaluation variables
-    total_timesteps = 50_000
+    total_timesteps = 2_000_000
     evaluation_freq = 1000
     n_episodes_to_evaluate = 20
 
