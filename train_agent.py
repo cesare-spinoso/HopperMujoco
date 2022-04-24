3501b32ee7ecbe777b71c59349027f4e5117208e
etiennedemers <74375914+etiennedemers@users.noreply.github.com>
2022-04-24 18:40:23 -0400

    change eval freq

--- train_agent.py
+++ train_agent.py
@@ -131,8 +131,8 @@ if __name__ == "__main__":
 
     # Training and evaluation variables
     total_timesteps = 2_000_000
-    evaluation_freq = 1000
-    n_episodes_to_evaluate = 20
+    evaluation_freq = 10000
+    n_episodes_to_evaluate = 10
 
     ########################################## training a single/multiple agent(s) ##########################################
     # starting a logger - results stored in folder labeled w/ date+time
