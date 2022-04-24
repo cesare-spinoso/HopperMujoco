f7381f7c2d80900a4a7b7ed8eb72829be39b2828
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-24 15:22:57 -0400

    save

--- openai_sac_agent/best_hyperparameters.py
+++ openai_sac_agent/best_hyperparameters.py
@@ -9,9 +9,5 @@ params = {
     "q_activation_function": torch.nn.ReLU,
     "policy_architecture": (64, 64),
     "policy_activation_function": torch.nn.ReLU,
-    "update_alpha": True, # Automatic exploration
-    "exploration_timesteps": 0, # Don't forget
-    "update_start_in_episodes": 50,
-    "update_frequency_in_episodes": 5
 }
 # the rest are kept as default
