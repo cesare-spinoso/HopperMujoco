d19dcb9e037f111b567c29d53a4009155d02f3b8
etiennedemers <74375914+etiennedemers@users.noreply.github.com>
2022-04-25 12:27:26 -0400

    .

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -4,6 +4,7 @@ import numpy as np
 def evaluate_agent(agent, env, n_episodes_to_evaluate):
     """Evaluates the agent for a provided number of episodes."""
     array_of_acc_rewards = []
+    ep_lens = []
     for _ in range(n_episodes_to_evaluate):
         acc_reward = 0
         done = False
@@ -14,5 +15,8 @@ def evaluate_agent(agent, env, n_episodes_to_evaluate):
             next_obs, reward, done, _ = env.step(action)
             acc_reward += reward
             curr_obs = next_obs
+            ep_len += 1
+        ep_lens.append(ep_len)
         array_of_acc_rewards.append(acc_reward)
+    print(f"mean episode len during eval: {np.mean(np.array(ep_lens))}"
     return np.mean(np.array(array_of_acc_rewards))
