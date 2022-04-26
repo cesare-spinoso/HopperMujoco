5aff25f66a07de12ed1c6401c278c7cf78179db5
Etienne Demers <etiennec.demers@gmail.com>
2022-04-26 12:57:58 -0400

    clean up

--- utils/environment.py
+++ utils/environment.py
@@ -9,10 +9,6 @@ def get_environment(env_type):
         env = JellyBeanEnv(gym.make("JBW-COMP579-obj-v1"))
     elif "mujoco" in env_type:
         env = MujocoEnv(gym.make("Hopper-v2"))
-    elif "ant" in env_type:
-        env = MujocoEnv(gym.make("Ant-v2"))
-    elif "walker" in env_type:
-        env = MujocoEnv(gym.make("Walker2d-v2"))
     else:
         raise Exception(
             "ERROR: Please define your env_type to be either 'jellybean' or 'mujoco'!"
