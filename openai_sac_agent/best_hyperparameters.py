89d2369523d05b4e3eff4d5e5e09c73e20463a51
etiennedemers <74375914+etiennedemers@users.noreply.github.com>
2022-04-26 18:41:11 -0400

    temporary

--- openai_sac_agent/best_hyperparameters.py
+++ openai_sac_agent/best_hyperparameters.py
@@ -24,6 +24,7 @@ params = {
     "policy_lr": 1e-3,
     "update_alpha": True,  # Automatic exploration
     "exploration_timesteps": 0,  # Don't get data that cause you to forget
+    "update_start_in_episodes": 50,
     #"update_start_in_timesteps": 5000,
     "update_frequency_in_episodes": 5,  # Increase update frequency
 }
