93c50acdd00c52e6ecb466e3d89d8ead25db4848
cesare-spinoso <cesare.spinoso@gmail.com>
2022-04-27 20:26:32 -0400

    save

--- openai_sac_agent/sample_efficient_hyperparameters.py
+++ openai_sac_agent/sample_efficient_hyperparameters.py
@@ -45,44 +45,14 @@ grid = [
         "policy_activation_function": [torch.nn.ReLU],
         "q_lr": [1e-3],
         "policy_lr": [1e-3],
-        "alpha": [0.8, 1.6, 2.4],
+        "alpha": [0.6, 0.7, 0.8, 0.9],
+        "update_alpha": [None],
         "exploration_timesteps": [100],
         "update_start_in_episodes": [100],
         "update_frequency_in_episodes": [1],
-        "replay_buffer_type": ["prioritized"]	
-    },
-    {
-        "q_architecture": [(64, 64)],
-        "q_activation_function": [torch.nn.ReLU],
-        "policy_architecture": [(64, 64)],
-        "policy_activation_function": [torch.nn.ReLU],
-        "q_lr": [1e-3],
-        "policy_lr": [1e-3],
-        "alpha": [1.6, 2.4],
-        "exploration_timesteps": [100],
-        "update_start_in_episodes": [100],
-        "update_frequency_in_episodes": [1],
-        "replay_buffer_type": ["uniform"]
-    },
-]
-
-# The hyperparamters chosen for the sample efficient agent
-# grid = {
-#     "q_architecture": [(64, 64)],
-#     "q_activation_function": [torch.nn.ReLU],
-#     "policy_architecture": [(64, 64)],
-#     "policy_activation_function": [torch.nn.ReLU],
-#     "q_lr": [1e-3],
-#     "policy_lr": [1e-3],
-#     "alpha": [0.8],
-#     "update_alpha": [False],
-#     "exploration_timesteps": [100],
-#     "update_start_in_episodes": [100],
-#     "update_frequency_in_episodes": [1],
-# }
+        "replay_buffer_type": "uniform"
+}
 
 hyperparameter_grid = []
 for i, params in enumerate(sklearn.model_selection.ParameterGrid(grid)):
-    if i == 0:
-        continue
     hyperparameter_grid.append(params)
